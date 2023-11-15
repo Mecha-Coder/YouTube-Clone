d6f5600aa3e9b3a2fa26e962e020478717230b95
Jason Selau Kumar <148664294+JasonSK999@users.noreply.github.com>
2023-11-15 14:57:02 +0800

    Rev 01

---
+++ Script/clicks.js
@@ -0,0 +1,98 @@
+let focusState = false;
+
+//-------------------| Left |----------------------
+const hamberger = document.getElementById("hamberger");
+const logo = document.getElementById("youtube-logo");
+const back = document.getElementById("back");
+
+//-----------------| Center |----------------------
+
+const centerHeader = document.getElementById("header-center");
+const inputText = document.getElementById("input");
+const clearText = document.getElementById("del");
+const searchBar = document.getElementById("searchbar");
+const microphone = document.getElementById("microphone");
+
+//-----------------| Right |----------------------
+
+const magGlass = document.getElementById("mag-glass");
+const mic = document.getElementById("mic");
+const dots = document.getElementById("dots");
+const signIn = document.getElementById("sign-in");
+
+// Display "X" icon when there is text inside the searchbox
+inputText.addEventListener("keyup", () => {
+  CheckInput();
+});
+
+// Clear all text inside searchbox
+clearText.addEventListener("click", () => {
+  inputText.value = "";
+  CheckInput();
+  inputText.focus();
+});
+
+// Small screen: When click unhide header-center
+magGlass.addEventListener("click", () => {
+  SearchFocus(true);
+});
+
+document.addEventListener("click", (event) => {
+  let getFocus = document.activeElement.getAttribute("id");
+  if (focusState && getFocus !== "input") {
+    SearchFocus(false);
+  }
+});
+
+// Script --------------------------------------------------------
+
+function CheckInput() {
+  if (inputText.value !== "") {
+    searchBar.style.paddingRight = "0px";
+    clearText.style.display = "flex";
+  } else {
+    searchBar.style.paddingRight = "4px";
+    clearText.style.display = "none";
+  }
+}
+
+function SearchFocus(state) {
+  let a, b;
+  focusState = state;
+
+  if (state) {
+    a = "hide";
+    b = "show";
+  } else {
+    a = "show";
+    b = "hide";
+  }
+
+  back.classList.remove(a);
+  back.classList.add(b);
+
+  centerHeader.classList.remove(a);
+  centerHeader.classList.add(b);
+  //--
+  logo.classList.add(a);
+  logo.classList.remove(b);
+
+  hamberger.classList.add(a);
+  hamberger.classList.remove(b);
+
+  microphone.classList.add(a);
+  microphone.classList.remove(b);
+
+  magGlass.classList.add(a);
+  magGlass.classList.remove(b);
+
+  dots.classList.add(a);
+  dots.classList.remove(b);
+
+  signIn.classList.add(a);
+  signIn.classList.remove(b);
+
+  if (state) {
+    inputText.focus();
+  }
+}

43805002c974c15b5189602d6c7f36ccf0e44a22
Jason Selau Kumar <148664294+JasonSK999@users.noreply.github.com>
2023-11-16 18:01:05 +0800

    load-video-from-json

--- Script/load.js
+++ Script/load.js
@@ -3,9 +3,46 @@ fetch("../Data/Content/video-database.json")
   .then((videoData) => {
     let tagList = [];
 
-    videoData.forEach((video) => {
-      const array = video.tag;
+    const videogrid = document.getElementById("video-grid");
 
+    videoData.forEach((video) => {
+      // Display each video ---------------------------------
+
+      videogrid.innerHTML += `
+      <div class="video-card">
+
+        <div class="thumbnail">
+          <a href="https://www.youtube.com/watch?v=${video.videoId}">
+            <img src="Data/Content/videos/${video.videoId}.webp" />
+          </a>
+      
+          <div class="video-duration">${video.duration}</div>
+        </div>
+      
+        <div class="video-detail">
+          <img src="" />
+      
+          <div class="text-detail">
+            <a class="video-title" href="https://www.youtube.com/watch?v=${
+              video.videoId
+            }">
+              ${video.title}
+            </a>
+            <a
+              href=""
+              class="channel-name"
+            >
+              ${"Blank"}
+            </a>
+            <div class="statistic">${video.views} views &middot; ${
+        video.upload
+      } ago</div>
+          </div>
+        </div>
+      </div>`;
+
+      // Get tag list  ---------------------------------------
+      const array = video.tag;
       array.forEach((tag) => {
         if (!tagList.includes(tag)) {
           tagList.push(tag);
@@ -13,7 +50,7 @@ fetch("../Data/Content/video-database.json")
       });
     });
 
-    // Load to page
+    // Load tag to page
     const subHeader = document.getElementById("sub-header");
     const button = document.getElementById("next-container");
 
